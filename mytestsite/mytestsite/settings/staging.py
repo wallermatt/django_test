842157923d353d51e42e1461ce182def99840d08
Matthew Waller <matthew.waller@qad.re>
2022-03-01 23:09:34 +0000

    static

--- mytestsite/mytestsite/settings/staging.py
+++ mytestsite/mytestsite/settings/staging.py
@@ -131,6 +131,10 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+print(STATIC_ROOT)
+print('****', STATIC_URL)
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
